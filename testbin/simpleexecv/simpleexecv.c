77b31aee33b28245e65cc72c4e7e4a7b4daaca15
Ryan Sears <rsears2@zeus-1.vse.gmu.edu>
2021-04-30 00:58:36 -0400

    minor changes

--- testbin/simpleexecv/simpleexecv.c
+++ testbin/simpleexecv/simpleexecv.c
@@ -18,9 +18,25 @@ static char *add_argv[3] = { (char *)"5", (char *)"7", NULL };
 int
 main()
 {
-	warn("SIMPLE EXECV\n");
+	int pid;
+	int status;
 
-	execv("/testbin/add", add_argv);	
+	printf("SIMPLE EXECV\n");
+
+	pid = fork();	
+
+	if(pid == 0) {
+		printf("CHILD: calling execv\n");
+		execv("/testbin/add", add_argv);
+	}	
+	else if(pid > 0) {
+		printf("PARENT: wait\n");
+		waitpid(pid, &status, 0);
+		printf("PARENT: waited\n");
+	}
+	else {
+		printf("ERROR: fork\n");
+	}
 
 	return 0;
 }
